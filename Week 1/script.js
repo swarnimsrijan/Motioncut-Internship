099255cbc60f92053c6ab62afafae713854983a8
Swarnim Srijan <1142srijan2002@gmail.com>
2023-11-13 16:04:33 +0530

    Update script.js

--- Week 1/script.js
+++ Week 1/script.js
@@ -29,74 +29,20 @@ const sr = ScrollReveal ({
   sr.reveal('.food-main-content',{ delay:350, origin:'bottom' })
   sr.reveal('.before',{ delay:350, origin:'bottom' })
   sr.reveal('.footer-logo',{ delay:350, origin:'bottom' })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let menubar = document.querySelector('#menubar');
-// let mynav = document.querySelector('.navbar');
-
-
-
-// menubar.onclick = () =>{
-//     menubar.classList.toggle('fa-times')
-//     mynav.classList.toggle('active')
-// }
-
-// document.querySelector('#loginbtn').onclick = () =>{
-//     document.querySelector('.login-form').classList.toggle('active');
-//   }
-//   document.querySelector('#closeloginform').onclick = () =>{
-//     document.querySelector('.login-form').classList.remove('active');
-//   }
+  
+  let menubar = document.querySelector('#menubar');  
+  let mynav = document.querySelector('.navbar');  
+    
+    
+    
+  menubar.onclick = () =>{  
+      menubar.classList.toggle('fa-times')  
+      mynav.classList.toggle('active')  
+  }  
+    
+  document.querySelector('#loginbtn').onclick = () =>{  
+      document.querySelector('.login-form').classList.toggle  ('active');
+    }  
+    document.querySelector('#closeloginform').onclick = () =  >{
+      document.querySelector('.login-form').classList.remove  ('active');
+    }  
